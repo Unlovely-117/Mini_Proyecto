0ae959a6bced9c740eb7ae8a94d9ec887ecec090
KevinBermeo0318 <kevin.bermeo@correounivalle.edu.co>
2025-09-26 19:38:42 -0500

    Clase abstracta Personsaje

---
+++ Miniproyecto_1/src/Personaje.java
@@ -0,0 +1,42 @@
+public abstract class Personaje {
+    protected string nombre;
+    protected int hpMax;
+    protected int hpActual;
+    protected int mpMax;
+    protected int mpActual;
+    protected int ataque;
+    protected int defensa;
+    protected int velocidad;
+    protected boolean vivo = true;
+
+    public Personaje(string nombre, int hpMax, int mpMax, int ataque, int defensa, int velocidad) {
+        this.nombre = nombre;
+        this.hpMax = hpActual = hpMax;
+        this.mpMax = mpActual = mpMax;
+        this.ataque = ataque;
+        this.defensa = defensa;
+        this.velocidad = velocidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public int getHpActual() {
+        return hpActual;
+    }
+    public int getHpMax() {
+        return hpMax;
+    }
+    public int getMpActual() {
+        return mpActual;
+    }
+
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    public boolean isVivo() {
+        return vivo;
+    }
+}
+
