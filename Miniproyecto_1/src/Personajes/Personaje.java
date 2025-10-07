d5da91728b11f587c6fe8453b40837f34e6b9b44
KevinBermeo0318 <kevin.bermeo@correounivalle.edu.co>
2025-10-07 11:13:05 -0500

    Clase abstracta Personsaje

---
+++ Miniproyecto_1/src/Personajes/Personaje.java
@@ -0,0 +1,42 @@
+package Personajes;
+
+import Habilidades.Habilidad;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Personaje {
+    protected String nombre;
+    protected int hp, mp, ataque, defensa, velocidad;
+    protected boolean vivo = true;
+    protected List<Habilidad> habilidades = new ArrayList<>();
+
+    public Personaje(String nombre, int hp, int mp, int ataque, int defensa, int velocidad) {
+        this.nombre = nombre;
+        this.hp = hp;
+        this.mp = mp;
+        this.ataque = ataque;
+        this.defensa = defensa;
+        this.velocidad = velocidad;
+    }
+
+    public String getNombre() { return nombre; }
+    public int getVelocidad() { return velocidad; }
+    public boolean estaVivo() { return vivo; }
+    public int getAtaque() {return ataque;}
+
+
+    public void recibirDaño(int daño) {
+        int dañoFinal = Math.max(0, daño - defensa);
+        hp -= dañoFinal;
+        if (hp <= 0) {
+            hp = 0;
+            vivo = false;
+        }
+        System.out.println(nombre + " recibe " + dañoFinal + " de daño. (HP: " + hp + ")");
+    }
+
+    public abstract void atacar(Personaje objetivo);
+}
+
+
+
